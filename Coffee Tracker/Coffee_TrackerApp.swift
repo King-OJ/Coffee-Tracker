25e7bd5afb26a35b8baaff304361c0d145f28a86
Clement Ojiguo <clementojigs@hotmail.com>
2023-09-14 15:07:38 +0100

    Initial Commit

---
+++ Coffee Tracker/Coffee_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_TrackerApp.swift
+//  Coffee Tracker
+//
+//  Created by Clem OJ on 14/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
